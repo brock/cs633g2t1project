18d910a639f83d535d14ba76717d4415f07be092
brockangelo <brockangelo@d65be8b6-0c61-82a8-fec6-4a0336c66c72>
2010-07-20 14:09:02 +0000

    Added all the documents from week 2 that were in Wiggio

---
+++ add.php
@@ -0,0 +1,92 @@
+<!-- Lars Jowers -->
+<!-- MET CS 633 -->
+<!-- Assignment 2 -->
+
+<!-- XHTML Heading -->
+<!--
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+<meta http-equiv="Content-Script-Type" content="text/javascript"/>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+
+<!-- Style sheet definition -->
+<link href="index.css" rel="stylesheet" type="text/css" />
+<title>
+Add Point to Cancer Maps
+</title>
+</head>
+
+<body>
+<!-- creates a buffer to center the add field on the page --> 
+	<div id="head" style="width: 500px; height: 50px"></div>
+		
+<!-- Uses formvalidate.js for the validate() function -->
+<!-- Uses table.form in CSS to format table for form -->
+<form id="form1" action="./registered.html" enctype="text/plain" method="post" onsubmit="return validate();">
+<table class="formpage">
+<caption>Add Point to Map</caption>
+<tr>
+<td colspan=2 align=center><b>Personal Information</b></td>
+<tr>
+<td><b>First Name:</b></td> <td><input type="text" size="20" id="firstname"/></td>
+</tr>
+<tr>
+<td><b>Last Name:</b></td> <td><input type="text" size="20" id="lastname"/></td>
+</tr>
+<tr>
+<td><b>Street Address:</b></td> <td><input type="text" size="30" id="address" /></td>
+</tr>
+<tr>
+<td><b>City:</b></td> <td><input type="text" size="30" id="city" /></td>
+</tr>
+<tr>
+<td><b>State:(e.g. FL or Florida)</b></td> <td><input type="text" size="30" id="state" /></td>
+</tr>
+<tr>
+<td><b>Zip: XXXXX</b></td> <td><input type="text" size="5" id="zip" /></td>
+</tr>
+<tr>
+<td><b>Email:</b></td> <td><input type="text" size="15" id="email" /></td>
+</tr>
+<tr>
+<tr>
+<td colspan=2 align=center><b>Medical Information</b></td>
+</tr>
+<tr>
+<tr>
+<td><b>Diagnosis Date:</b></td> <td><input type="text" size="15" id="diagdate" /></td>
+</tr>
+<tr>
+<!-- <td><b>Diagnosis Date:</b></td> <td><input type="text" size="15" id="diagdate" /></td> -->
+<td><b>Diagnosis Type:</b></td> 
+<td>
+<select name="diagtype">
+<option value="lung">Lung Cancer</option>
+<option value="breast">Breast Cancer</option>
+<option value="leukemia">Leukemia</option>
+<option value="hodgkins">Hodgkins</option>
+<option value="non-hodgkins">non-Hodgkins</option>
+<option value="colon">Colon Cancer</option>
+</select>
+</td>
+
+</tr>
+<caption>Medical Information</caption>
+<td colspan="2"><input type="submit" value="submit" /> or go <a href="../">Back to Map</a></td>
+</tr>
+</table>
+</form>
+
+
+</body>
+</html>
