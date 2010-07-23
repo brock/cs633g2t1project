3bab54bf4fa69f02c19373d22da1a824d176d4db
brockangelo <brockangelo@d65be8b6-0c61-82a8-fec6-4a0336c66c72>
2010-07-23 04:54:04 +0000

    Adding the formvalidate javascript file imported Lars' updates to the index.html into the add.php Redirected the add.php form back to add.php and made PHP check to see if the form was submitted. That way we don't have to add another page. Got rid of Hello and Test folders and the Word document. Added indentation to add.php so it was easier to read.

--- trunk/add.php
+++ trunk/add.php
@@ -15,76 +15,91 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
-<head>
-<meta http-equiv="Content-Script-Type" content="text/javascript"/>
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+	<head>
+		<meta http-equiv="Content-Script-Type" content="text/javascript"/>
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 
-<!-- Style sheet definition -->
-<link href="index.css" rel="stylesheet" type="text/css" />
-<title>
-Add Point to Cancer Maps
-</title>
-</head>
+		<!-- Style sheet definition -->
+		<link href="index.css" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="formvalidate.js"></script>
+		<title>
+			Add Point to Cancer Maps
+		</title>
+	</head>
 
 <body>
+
+<!-- if this page is being loaded after the user submits new data, confirm it or show an error -->
+<?php 	
+	// If the user submitted something that JavaScript approved...
+	if($_SERVER['REQUEST_METHOD'] == "POST") {		
+			// send the request in for geocoding
+			// accept the geocoding results
+			// if good, show a nice update
+			echo "<center>You submitted something. But we aren't doing anything with it yet.</center>";
+			// if it fails, display the error message
+			}
+?>
+
 <!-- creates a buffer to center the add field on the page --> 
-	<div id="head" style="width: 500px; height: 50px"></div>
+<div id="head" style="width: 500px; height: 50px"></div>
 		
 <!-- Uses formvalidate.js for the validate() function -->
 <!-- Uses table.form in CSS to format table for form -->
-<form id="form1" action="./registered.html" enctype="text/plain" method="post" onsubmit="return validate();">
-<table class="formpage">
-<caption>Add Point to Map</caption>
-<tr>
-<td colspan=2 align=center><b>Personal Information</b></td>
-<tr>
-<td><b>First Name:</b></td> <td><input type="text" size="20" id="firstname"/></td>
-</tr>
-<tr>
-<td><b>Last Name:</b></td> <td><input type="text" size="20" id="lastname"/></td>
-</tr>
-<tr>
-<td><b>Street Address:</b></td> <td><input type="text" size="30" id="address" /></td>
-</tr>
-<tr>
-<td><b>City:</b></td> <td><input type="text" size="30" id="city" /></td>
-</tr>
-<tr>
-<td><b>State:(e.g. FL or Florida)</b></td> <td><input type="text" size="30" id="state" /></td>
-</tr>
-<tr>
-<td><b>Zip: XXXXX</b></td> <td><input type="text" size="5" id="zip" /></td>
-</tr>
-<tr>
-<td><b>Email:</b></td> <td><input type="text" size="15" id="email" /></td>
-</tr>
-<tr>
-<tr>
-<td colspan=2 align=center><b>Medical Information</b></td>
-</tr>
-<tr>
-<tr>
-<td><b>Diagnosis Date:</b></td> <td><input type="text" size="15" id="diagdate" /></td>
-</tr>
-<tr>
-<!-- <td><b>Diagnosis Date:</b></td> <td><input type="text" size="15" id="diagdate" /></td> -->
-<td><b>Diagnosis Type:</b></td> 
-<td>
-<select name="diagtype">
-<option value="lung">Lung Cancer</option>
-<option value="breast">Breast Cancer</option>
-<option value="leukemia">Leukemia</option>
-<option value="hodgkins">Hodgkins</option>
-<option value="non-hodgkins">non-Hodgkins</option>
-<option value="colon">Colon Cancer</option>
-</select>
-</td>
-
-</tr>
-<caption>Medical Information</caption>
-<td colspan="2"><input type="submit" value="submit" /> or go <a href="../">Back to Map</a></td>
-</tr>
-</table>
+<!-- add.php handles form submission -->
+<form name="add_form" id="form1" action="add.php" enctype="text/plain" method="post" onsubmit="return validate();">
+	<table class="formpage">
+		<caption>Add Point to Map</caption>
+		<tr>
+			<td colspan=2 align=center><b>Personal Information</b></td>
+		<tr>
+			<td><b>First Name:</b></td> <td><input type="text" size="20" id="firstname"/></td>
+		</tr>
+		<tr>
+			<td><b>Last Name:</b></td> <td><input type="text" size="20" id="lastname"/></td>
+		</tr>
+		<tr>
+			<td><b>Street Address:</b></td> <td><input type="text" size="30" id="address" /></td>
+		</tr>
+		<tr>
+			<td><b>City:</b></td> <td><input type="text" size="30" id="city" /></td>
+		</tr>
+		<tr>
+			<td><b>State:(e.g. FL or Florida)</b></td> <td><input type="text" size="30" id="state" /></td>
+		</tr>
+		<tr>
+			<td><b>Zip: XXXXX</b></td> <td><input type="text" size="5" id="zip" /></td>
+		</tr>
+		<tr>
+			<td><b>Email:</b></td> <td><input type="text" size="15" id="email" /></td>
+		</tr>
+		<tr>
+		<tr>
+			<td colspan=2 align=center><b>Medical Information</b></td>
+		</tr>
+		<tr>
+			<caption>Medical Information</caption>
+			<tr>
+				<td><b>Diagnosis Date:</b></td> <td><input type="text" size="15" id="diagdate" /></td>
+			</tr>
+			<tr>
+				<!-- can this next line be removed? -->
+				<!-- <td><b>Diagnosis Date:</b></td> <td><input type="text" size="15" id="diagdate" /></td> -->
+				<td><b>Diagnosis Type:</b></td> 
+				<td>
+				<select name="diagtype">
+				<option value="lung">Lung Cancer</option>
+				<option value="breast">Breast Cancer</option>
+				<option value="leukemia">Leukemia</option>
+				<option value="hodgkins">Hodgkins</option>
+				<option value="non-hodgkins">non-Hodgkins</option>
+				<option value="colon">Colon Cancer</option>
+				</select>
+				</td>
+			</tr>
+			<td colspan="2"><input type="submit" value="submit" /> or go <a href="../">Back to Map</a></td>
+		</tr>
+	</table>
 </form>
 
 
