74a385ee9c30baa40c479dbeeb754d08d4340307
mike.nithaworn@gmail.com <mike.nithaworn@gmail.com@d65be8b6-0c61-82a8-fec6-4a0336c66c72>
2010-07-24 09:15:03 +0000

    Adding a geocoding test file geocoding.php that was successful.  This code looks up my given address and echos the longitude and latitude if you run the script.

---
+++ trunk/geocode.php
@@ -0,0 +1,33 @@
+<?php
+define("MAPS_HOST", "maps.google.com");
+
+//this is a my own personal key
+define("KEY", "ABQIAAAAR3CaSE2qqeWGkC56FEsbQRTPbgwNZ2RKRODPuAlSED3BkqR1ExT5er_UOINhI1C2R3cVPWZmbVNDAw");
+
+$base_url = "http://" . MAPS_HOST . "/maps/geo?output=xml" . "&key=" . KEY;
+
+//my address
+$address = "8730 Costa Verde Blvd San Diego, CA";
+$request_url = $base_url . "&q=" . urlencode($address);
+
+//xml request
+$xml = simplexml_load_file($request_url) or die("url not loading");
+
+//get status from xml response
+$status = $xml->Response->Status->code;
+
+//if response is 200 then it is good
+echo "Response status is: " . $status . "\n";
+
+if (strcmp($status, "200") == 0) {
+	$coordinates = $xml->Response->Placemark->Point->coordinates;
+	$coordinatesSplit = split(",", $coordinates);
+	
+	// Format: Longitude, Latitude, Altitude
+	$lat = $coordinatesSplit[1];
+	$lng = $coordinatesSplit[0];
+
+	echo "The latitude is: " . $lat . "\n";	
+	echo "The longitude is: " . $lng . "\n";
+}
+?>
